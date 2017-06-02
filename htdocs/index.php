ec3ea3c903d92750694bfe86f92d16aea34cadd5
Hiroaki Komatsu <komatsu@syake-labo.com>
2017-06-02 19:54:56 +0900

    cakephp インストール

---
+++ htdocs/index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The Front Controller for handling every request
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         0.2.9
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+// for built-in server
+if (php_sapi_name() === 'cli-server') {
+    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
+
+    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
+    $file = __DIR__ . $url['path'];
+    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
+        return false;
+    }
+}
+$cakephp_path = realpath(dirname(__FILE__) . '/../cakephp');
+require $cakephp_path . '/vendor/autoload.php';
+
+use App\Application;
+use Cake\Http\Server;
+
+// Bind your application to the server.
+$server = new Server(new Application($cakephp_path . '/config'));
+
+// Run the request/response through the application
+// and emit the response.
+$server->emit($server->run());

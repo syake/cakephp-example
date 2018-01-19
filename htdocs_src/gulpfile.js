9f7fc607c66babd0ef75bcd871c4772350e2d75d
Hiroaki Komatsu <komatsu@syake-labo.com>
2018-01-19 16:00:30 +0900

    BrowserSyncインストール

--- htdocs_src/gulpfile.js
+++ htdocs_src/gulpfile.js
@@ -1,3 +1,10 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default',[]);
+gulp.task('serve', function() {
+  browserSync.init({
+    proxy: 'cakephp:8888'
+  });
+});
+
+gulp.task('default',['serve']);

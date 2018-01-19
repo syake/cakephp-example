353597a5587c12c8132446d31c1029e021f71ebb
Hiroaki Komatsu <komatsu@syake-labo.com>
2018-01-19 16:16:58 +0900

    gulp-sassインストール

--- htdocs_src/gulpfile.js
+++ htdocs_src/gulpfile.js
@@ -1,10 +1,26 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
+// CSS
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
+
+var src = 'src';
+var dst = '../htdocs';
+
+gulp.task('sass', function () {
+  gulp.src(src+'/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(dst+'/css'))
+})
 gulp.task('serve', function() {
   browserSync.init({
     proxy: 'cakephp:8888'
   });
 });
 
-gulp.task('default',['serve']);
+gulp.task('default',['sass','serve']);
